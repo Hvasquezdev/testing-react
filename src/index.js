60bdb2099e37169d6d4a9ae64a9942ab3d436d3c
Hector Vasquez <35814981+Hvasquezdev@users.noreply.github.com>
2018-10-16 21:57:11 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function formatDate(date) {
+  return date.toLocaleString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64"
+  }
+};
+
+const element = (
+  <Comment date={comment.date} text={comment.text} author={comment.author} />
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(element, rootElement);
