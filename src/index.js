7103600dec804d72723ac5a6545a39e2372cc7bb
Hector Vasquez <35814981+Hvasquezdev@users.noreply.github.com>
2018-10-18 21:22:19 -0400

    Making Square Component Interactive

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import "./styles.css";
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => alert("click")}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
